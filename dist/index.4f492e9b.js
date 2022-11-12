1932d7739caf1e90b85312966ffeb8659465a0b3
pedrovega1 <luidzhi.vegas@mail.ru>
2022-11-12 14:00:15 +0600

    calc

--- dist/index.4f492e9b.js
+++ dist/index.4f492e9b.js
@@ -1,53 +1,21 @@
-var randomNumber = Math.floor(Math.random() * 100) + 1;
-var guesses = document.querySelector(".guesses");
-var lastResult = document.querySelector(".lastResult");
-var lowOrHi = document.querySelector(".lowOrHi");
-var guessSubmit = document.querySelector(".guessSubmit");
-var guessField = document.querySelector(".guessField");
-var guessCount = 1;
-var resetButton;
-function checkGuess() {
-    var userGuess = Number(guessField.value);
-    if (guessCount === 1) guesses.textContent = "Previous guesses: ";
-    guesses.textContent += userGuess + " ";
-    if (userGuess === randomNumber) {
-        lastResult.textContent = "Congratulations! You got it right!";
-        lastResult.style.backgroundColor = "green";
-        lowOrHi.textContent = "";
-        setGameOver();
-    } else if (guessCount === 10) {
-        lastResult.textContent = "!!!GAME OVER!!!";
-        setGameOver();
-    } else {
-        lastResult.textContent = "Wrong!";
-        lastResult.style.backgroundColor = "red";
-        if (userGuess < randomNumber) lowOrHi.textContent = "Last guess was too low!";
-        else if (userGuess > randomNumber) lowOrHi.textContent = "Last guess was too high!";
+const calc = document.querySelector(".calc");
+const result = document.querySelector("#result");
+calc.addEventListener("click", function(event) {
+    // Метод contains объекта classList проверяет наличие  класса элемента.
+    if (!event.target.classList.contains("calc_btn")) return;
+    // создаем постоянную переменную value, где она будет позволяющее задавать или получать текстовое содержимое элемента и его потомков (Innertext)
+    const value = event.target.innerText;
+    switch(value){
+        case "C":
+            result.innerText = "";
+            break;
+        case "=":
+            result.innerText = eval(result.innerText).toFixed(2);
+            break;
+        default:
+            result.innerText += value;
     }
-    guessCount++;
-    guessField.value = "";
-    guessField.focus();
-}
-guessSubmit.addEventListener("click", checkGuess);
-function setGameOver() {
-    guessField.disabled = true;
-    guessSubmit.disabled = true;
-    resetButton = document.createElement("button");
-    resetButton.textContent = "Start new game";
-    document.body.appendChild(resetButton);
-    resetButton.addEventListener("click", resetGame);
-}
-function resetGame() {
-    guessCount = 1;
-    var resetParas = document.querySelectorAll(".resultParas p");
-    for(var i = 0; i < resetParas.length; i++)resetParas[i].textContent = "";
-    resetButton.parentNode.removeChild(resetButton);
-    guessField.disabled = false;
-    guessSubmit.disabled = false;
-    guessField.value = "";
-    guessField.focus();
-    lastResult.style.backgroundColor = "white";
-    randomNumber = Math.floor(Math.random() * 100) + 1;
-}
+    console.log(event.target);
+}); // event.target - получить элемент, на котором произошло событие, и подсветить его.(тут уже показать в консоли)
 
 //# sourceMappingURL=index.4f492e9b.js.map
